f504e9561815164bb69cd6f46bcb6fc968904fea
yt0001 <2379168510@qq.com>
2021-10-30 16:50:01 +0800

    retrict test

---
+++ productor_consumer_model/restrict.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+ 
+int foo(int *a, int *b)
+{
+    *a = 5;
+    *b = 6;
+    return *a + *b;
+}
+ 
+int rfoo(int *restrict a, int *restrict b)
+{
+    *a = 5;
+    *b = 6;
+    return *a + *b;
+}
+ 
+int main(void)
+{
+    int i =0;
+    int *a = &i;
+    int *b = &i;
+ 
+    printf("%d ",foo(a,b));    // 输出 12
+    printf("%d ", rfoo(a,b));  // 可能输出 11
+    return 0;
+}
