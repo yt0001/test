a9cdf4ae2004a33a063d8556358d44149c21f804
yt0001 <2379168510@qq.com>
2021-11-02 10:04:35 +0800

    add more details

--- productor_consumer_model/restrict.c
+++ productor_consumer_model/restrict.c
@@ -25,3 +25,35 @@ int main(void)
     printf("%d ", rfoo(a,b));  // 可能输出 11
     return 0;
 }
+/*优化前s
+ 
+int ar[10];
+int* par = ar;    //既可以通过par也可以通过ar访问本数组
+ 
+restrict int* p1 = (int*)malloc(10 * sizeof(int));
+ 
+for(int i = 0; i < 10; ++i)
+{
+    par[i] += 5;    //语句1
+    p1[i] += 5;     //语句2
+    arr[i] *= 2;    //语句3
+    par[i] += 3;    //语句4
+    p1[i] += 3;    //语句5
+}  
+ */
+
+/*优化后
+
+ int ar[10];
+int* par = ar;    //既可以通过par也可以通过ar访问本数组
+ 
+restrict int* p1 = (int*)malloc(10 * sizeof(int));
+ 
+for(int i = 0; i < 10; ++i)
+{
+    par[i] += 5;    //语句1
+    p1[i] += 8;     //语句2
+    arr[i] *= 2;    //语句3
+    par[i] += 3;    //语句4
+}  
+ */
